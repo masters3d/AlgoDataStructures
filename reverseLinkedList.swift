14300b5fddd2c92ea062c3627fcf30cb1b2aaf15
Cheyo Jimenez <masters3d@users.noreply.github.com>
2018-02-12 23:36:43 -0800

    Create reverseLinkedList.swift

---
+++ reverseLinkedList.swift
@@ -0,0 +1,69 @@
+
+// Node in a linked list
+class Node<T>{
+    var value:T?
+    var next:Node<T>?
+}
+
+let root = Node<Int>()
+root.value = 0
+
+let one = Node<Int>()
+one.value = 1
+
+let two = Node<Int>()
+two.value = 2
+
+let three = Node<Int>()
+three.value = 3
+
+let four = Node<Int>()
+four.value = 4
+
+
+root.next = one
+one.next = two
+two.next = three
+three.next = four
+
+
+func printNode<T>(_ that: Node<T>?){
+    var that = that
+    while let this = that, let value = this.value{
+        print(value)
+        that = this.next
+    }
+}
+
+
+func reverse<T>(_ nodeIn:Node<T>?) -> Node<T>? {
+    var next = nodeIn
+    var newRoot:Node<T>?
+    while let currentNext = next {
+        let this = Node<T>()
+        this.next = newRoot
+        this.value = currentNext.value
+
+        next = currentNext.next
+        newRoot = this
+    }
+    return newRoot
+}
+
+let returned = reverse(root)
+
+printNode(root)
+print("---")
+printNode(returned)
+
+//0
+//1
+//2
+//3
+//4
+//---
+//4
+//3
+//2
+//1
+//0
